81563a77c8a104d26ffaff8d263eb343f2e89f53
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-17 19:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
-	if(s===' ')
-		return s;
-	const ab=s.indexOf(' ');
-	if(ab==-1)
-		return s;
-	return s.substring(0,ab);
+if (str==' ') {
+    return '';  
+  }
+
+  // Trim leading spaces
+  str = str.trim();
+  
+  // Find the index of the first space
+  const firstSpaceIndex = str.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return str;
+  }
+
+  // Return the substring from the start to the first space
+  return str.substring(0, firstSpaceIndex);
 	
 }
 
