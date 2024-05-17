bc6670e7374a4c7afdb9fc54d0e47de86f199d72
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-17 09:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
 	const ab=s.indexOf(' ');
 	if(ab==-1)
 		return s;
@@ -9,5 +10,5 @@ function firstWord(s) {
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
